51630ca38b5a546f70eca2100b195990119a04cd
Jerome Sonnet (be-cloud.be) <jerome.sonnet@be-cloud.be>
2018-09-19 05:57:22 +0000

    hide drawer when ready

--- v9c_backend_theme/static/src/js/web_responsive.js
+++ v9c_backend_theme/static/src/js/web_responsive.js
@@ -92,13 +92,6 @@ odoo.define('web_responsive', function(require) {
             $clickZones.click($.proxy(this.handleClickZones, this));
             core.bus.on('resize', this, this.handleWindowResize);
             core.bus.on('keydown', this, this.handleNavKeys);
-            
-            this.loading = false;
-            this.count = 0;
-            this.blocked_ui = false;
-            session.on("request", this, this.request_call);
-            session.on("response", this, this.response_call);
-            session.on("response_failed", this, this.response_call);
         },
 
         // It provides initialization handlers for Drawer
@@ -126,52 +119,15 @@ odoo.define('web_responsive', function(require) {
             var self = this;
             // TODO - hide when load is done
             //setTimeout(function() { self.$el.drawer('close'); }, 1200);
+            session.on("view_shown", this, function() {
+                this.$el.drawer('close');
+            });
             this.loading = true;
             $('.oe_secondary_menus_container')
                 .parent()
                 .collapse('hide');
         },
         
-        destroy: function() {
-            this.on_rpc_event(-this.count);
-            this._super();
-        },
-        request_call: function() {
-            if (this.loading) {
-                console.log('+1');
-                this.on_rpc_event(1);
-            }
-        },
-        response_call: function() {
-            if (this.loading) {
-                console.log('-1');
-                this.on_rpc_event(-1);
-            }
-        },
-        on_rpc_event : function(increment) {
-            var self = this;
-            if (!this.count && increment === 1) {
-                // Block UI after 3s
-                this.long_running_timer = setTimeout(function () {
-                    self.blocked_ui = true;
-                    framework.blockUI();
-                }, 6000);
-            }
-    
-            this.count += increment;
-            if (this.count <= 0) {
-                console.log('hide');
-                this.count = 0;
-                clearTimeout(this.long_running_timer);
-                // Don't unblock if blocked by somebody else
-                if (self.blocked_ui) {
-                    this.blocked_ui = false;
-                    framework.unblockUI();
-                }
-                this.$el.drawer('close');
-            }
-        },
-
         // It resizes bootstrap dropdowns for screen
         handleWindowResize: function() {
             $('.dropdown-scrollable').css(
